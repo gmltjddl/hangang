a878b4a04bf83879fd3476bc183459a098bd4144
gmltjddl <gmltjd7886@gmail.com>
2023-04-30 08:56:31 +0900

    qna

--- myapp/react-app/src/component/Qna/Qnalist.js
+++ myapp/react-app/src/component/Qna/Qnalist.js
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useContext } from "react";
 import axios from "axios";
 import "./css/Qnalist.css";
+import Usercontext from '../../Usercontext';
 
 const Qnalist = () => {
   const [qnas, setQnas] = useState([]);
   const [showContent, setShowContent] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [qnasPerPage] = useState(20);
-
+  const user = useContext(Usercontext);
+  const [isEditing, setIsEditing] = useState(false); // 수정 중인지 여부를 추적하는 상태
+  const [selectedQna, setSelectedQna] = useState(null); // 선택된 QnA를 추적하는 상태
+  
   useEffect(() => {
     fetchqnas();
   }, []);
@@ -16,10 +20,11 @@ const Qnalist = () => {
     try {
       const response = await axios.get("http://localhost:8080/web/qnas");
       const result = response.data;
-      console.log("Server response:", result); // 추가: 콘솔에 서버 응답 출력
+      console.log(result);
+   
       if (result.status === "success" && Array.isArray(result.data)) {
         console.log(result.data);
-        setQnas(result.data.reverse());
+        setQnas(result.data);
       } else {
         console.error("Error fetching qna: Invalid data format");
       }
@@ -63,6 +68,37 @@ const Qnalist = () => {
 
     return <ul className="page-numbers">{pageNumbers}</ul>;
   };
+  const handleEditClick = (event, qna) => {
+    event.stopPropagation(); // 이벤트 버블링 중단
+    setIsEditing(true); // 수정 중으로 설정
+    setSelectedQna(qna); // 선택된 QnA 설정
+  };
+
+  const handleModalClose = () => {
+    setIsEditing(false); // 수정 중 상태를 해제
+    setSelectedQna(null); // 선택된 QnA 해제
+  };
+
+  
+  const handleDeleteClick = async (event, qnaNo) => {
+    event.stopPropagation(); // 이벤트 버블링 중단
+    try {
+      const response = await axios.delete(`http://localhost:8080/web/qnas/${qnaNo}`);
+      const result = response.data;
+      console.log(result);
+      console.log(qnaNo);
+   
+      if (result.status === "success") {
+        // Remove the deleted qna from the state
+        const updatedQnas = qnas.filter(qna => qna.no !== qnaNo);
+        setQnas(updatedQnas);
+      } else {
+        console.error("Error deleting qna");
+      }
+    } catch (error) {
+      console.error("Error deleting qna:", error);
+    }
+  };
 
   return (
     <>
@@ -71,6 +107,7 @@ const Qnalist = () => {
         <div className="qna-header-bar">
           <span className="qna-number-header">No</span>
           <span className="qna-title-header">제목</span>
+          <span className="qna-nick-header">작성자</span>
           <span className="qna-date-header">작성일</span>
         </div>
         <ul>
@@ -83,13 +120,20 @@ const Qnalist = () => {
               <div className="qna-header">
                 <span className="qna-number">{qna.no}. </span>
                 <span className="qna-title">{qna.title}</span>
+                <span className="qna-nick">{qna.writer.nickName}</span>
                 <span className="qna-date"> {qna.createdDate}</span>
-              </div>
+             
+                  </div>
               {showContent === index && (
                 <div className="qna-content">
-                  <p className="qna-nickname">{qna.nickName}</p>
                   {renderSeparator()}
                   <p className="qna-text">{qna.content}</p>
+                  {user.no === qna.writer.no && (
+                 <span className="qna-actions">
+                  <button className="qedit-btn" onClick={(event) => handleEditClick(event, qna.no)}></button>
+                  <button className="qdelete-btn" onClick={(event) => handleDeleteClick(event, qna.no)}></button>
+                  </span>
+                  )}
                 </div>
               )}
             </li>
